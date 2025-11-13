2734036495838f527ecd0c9848ebd6eefa9b4f64
Natalio-R <nataliorabasconavarro@gmail.com>
2025-11-13 18:48:03 +0100

    Fix vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,27 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactCompiler: true,
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...config.externals,
+        'resend',
+        '@react-email/render',
+        ({ request }, callback) => {
+          if (request.startsWith('../../emails')) {
+            return callback(null, { commonjs: request });
+          }
+          callback();
+        },
+      ];
+    }
+    
+    return config;
+  },
 };
 
 export default nextConfig;
